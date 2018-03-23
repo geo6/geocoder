2b2a9e82c27296cdd1f79834563606dcc2451c48
jbelien <dev@jbelien.be>
2018-03-23 16:23:09 +0000

    Apply fixes from StyleCI

--- src/Handler/HomeHandler.php
+++ src/Handler/HomeHandler.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
-use App\Middleware\ConfigMiddleware;
-use App\Middleware\DbAdapterMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Db\Sql\Expression;
-use Zend\Db\Sql\Sql;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Expressive\Router\RouterInterface;
 use Zend\Expressive\Template\TemplateRendererInterface;
