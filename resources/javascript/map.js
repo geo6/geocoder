07887c83001b0cc0ad24fc363e2097ca1779c2de
Jonathan Beliën <jbe@geo6.be>
2018-07-02 12:58:13 +0200

    Update map.js
    Upgrade to OpenLayers 5.

--- resources/javascript/map.js
+++ resources/javascript/map.js
@@ -1,21 +1,39 @@
 import 'ol/ol.css';
 
-import Map from 'ol/map';
-import Control from 'ol/control';
-import Attribution from 'ol/control/attribution';
-import ScaleLine from 'ol/control/scaleline';
-import Feature from 'ol/feature';
-import Point from 'ol/geom/point';
-import TileLayer from 'ol/layer/tile';
-import VectorLayer from 'ol/layer/vector';
-import Proj from 'ol/proj';
-import OSMSource from 'ol/source/osm';
-import VectorSource from 'ol/source/vector';
-import Style from 'ol/style/style';
-import CircleStyle from 'ol/style/circle';
-import RegularShapeStyle from 'ol/style/regularshape';
-import StrokeStyle from 'ol/style/stroke';
-import View from 'ol/view';
+import {
+    Feature,
+    Map,
+    View
+} from 'ol';
+import {
+    defaults as ControlDefaults,
+    Attribution,
+    ScaleLine
+} from 'ol/control';
+import {
+    Point
+} from 'ol/geom';
+import {
+    Tile as TileLayer,
+    Vector as VectorLayer
+} from 'ol/layer';
+import {
+    OSM as OSMSource,
+    Vector as VectorSource
+} from 'ol/source';
+import {
+    ATTRIBUTION as OSMSourceAttribution
+} from 'ol/source/OSM';
+import {
+    fromLonLat,
+    toLonLat
+} from 'ol/proj';
+import {
+    Circle,
+    RegularShape,
+    Stroke,
+    Style
+} from 'ol/style';
 
 window.app.location = new Feature({});
 
@@ -23,38 +41,38 @@ window.app.locationLayer = new VectorLayer({
     source: new VectorSource({}),
     style: [
         new Style({
-            image: new CircleStyle({
+            image: new Circle({
                 fill: null,
                 radius: 25,
-                stroke: new StrokeStyle({color: 'black', width: 10})
+                stroke: new Stroke({color: 'black', width: 10})
             })
         }),
         new Style({
-            image: new CircleStyle({
+            image: new Circle({
                 fill: null,
                 radius: 25,
-                stroke: new StrokeStyle({color: 'white', width: 5})
+                stroke: new Stroke({color: 'white', width: 5})
             })
         }),
         new Style({
-            image: new RegularShapeStyle({
+            image: new RegularShape({
                 angle: 0,
                 fill: null,
                 points: 4,
                 radius: 5,
                 radius2: 0,
-                stroke: new StrokeStyle({color: 'black', width: 2})
+                stroke: new Stroke({color: 'black', width: 2})
             })
         })
     ]
 });
 
 window.app.map = new Map({
-    controls: Control.defaults({attribution: false}).extend([new Attribution({collapsible: false}), new ScaleLine()]),
+    controls: ControlDefaults({attribution: false}).extend([new Attribution({collapsible: false}), new ScaleLine()]),
     layers: [
         new TileLayer({
             source: new OSMSource({
-                attributions: [OSMSource.ATTRIBUTION, 'Tiles courtesy of <a href="https://geo6.be/" target="_blank">GEO-6</a>'],
+                attributions: [OSMSourceAttribution, 'Tiles courtesy of <a href="https://geo6.be/" target="_blank">GEO-6</a>'],
                 url: 'https://tile.geo6.be/osmbe/{z}/{x}/{y}.png',
                 maxZoom: 18
             })
@@ -68,7 +86,7 @@ window.app.map = new Map({
     })
 });
 window.app.map.on('singleclick', function (event) {
-    var coordinates = Proj.toLonLat(event.map.getCoordinateFromPixel(event.pixel));
+    var coordinates = toLonLat(event.map.getCoordinateFromPixel(event.pixel));
     var zoom = event.map.getView().getZoom();
 
     window.app.fn.reverse(coordinates[0], coordinates[1], zoom);
@@ -85,7 +103,7 @@ window.app.fn.clearLocation = function() {
  *
  */
 window.app.fn.locate = function (lng, lat) {
-    window.app.location.setGeometry(new Point(Proj.fromLonLat([lng, lat])));
+    window.app.location.setGeometry(new Point(fromLonLat([lng, lat])));
     window.app.locationLayer.getSource().addFeature(window.app.location);
 
     window.app.map.getView().animate({
